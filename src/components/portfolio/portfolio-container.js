23ef6dc0f7008710d19b9e39034c1ea620817ed1
matthewricklefs <ricklefsmatthew1003@gmail.com>
2020-12-22 12:48:08 -0500

    Using .map() to return dynamically render portfolio items onto page

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,17 +3,29 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+        console.log("Portfolio container has rendered");
+    }
+
+    portfolioItems() {
+        const data = ["Quip", "Eventbrite", "Ministry Safe", "Swingaway"];
+
+        return data.map(item => {
+            return <PortfolioItem />;
+        })
+    }
+
     // State 
-    // Lifestyle hooks
+    // Lifecycle hooks
     render() {
         return (
 
             <div>
 
                 <h2>Portfolio items go here...</h2>
-            
-            
-            <PortfolioItem />
+                   
+                {this.portfolioItems()}
 
             </div>
         )
