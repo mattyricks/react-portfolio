c6175e2bce3792ea7e9c0c83d6c0de2e961d1071
matthewricklefs <ricklefsmatthew1003@gmail.com>
2020-12-21 19:15:34 -0500

    added portfolio components

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from 'moment';
+
+import PortfolioContainer from "./portfolio/portfolio-container";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Matthew Ricklef's Portfolio</h1>
+        
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
+        
+        <PortfolioContainer />
       </div>
     );
   }
---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+    // State 
+    // Lifestyle hooks
+    render() {
+        return (
+
+            <div>
+
+                <h2>Portfolio items go here...</h2>
+            
+            
+            <PortfolioItem />
+
+            </div>
+        )
+    }
+}
