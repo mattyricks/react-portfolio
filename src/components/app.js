7ece935c216ef8e3e87fed01331763ebbfdef6c7
matthewricklefs <ricklefsmatthew1003@gmail.com>
2020-12-23 17:44:17 -0500

    creating navigation component/ using ternary operator

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import moment from 'moment';
 
 import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationComponent from "./navigation/navigation-component";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationComponent />
         <h1>Matthew Ricklef's Portfolio</h1>
         
         <div>
