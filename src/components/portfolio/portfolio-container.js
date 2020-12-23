9c90390716fb031d535b5f04ceb784b4c88f8de0
matthewricklefs <ricklefsmatthew1003@gmail.com>
2020-12-23 16:51:25 -0500

    How to build a data filter in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -9,14 +9,22 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "Welcome to my portfolio",
             data: [ 
-                { title: "Quip" },
-                { title: "Eventbrite" }, 
-                { title: "Ministry Safe" },
-                { title: "Swingaway" }
+                { title: "Quip", category: "eCommerce" },
+                { title: "Eventbrite", category: "Scheduling" }, 
+                { title: "Ministry Safe", category: "Enterprise"},
+                { title: "Swingaway", category: "eCommerce" }
             ]
         };
 
-        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this)
+        this.handleFilter = this.handleFilter.bind(this)
+    }
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        })
     }
 
     portfolioItems() {
@@ -27,26 +35,19 @@ export default class PortfolioContainer extends Component {
         });
     }
 
-    handlePageTitleUpdate() {
-        this.setState({
-            pageTitle: "Somethin else"
-        })
-    }
-
-    // State 
-    // Lifecycle hooks
     render() {
         return (
 
             <div>
 
                 <h2>{this.state.pageTitle}</h2>
+
+                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
                    
                 {this.portfolioItems()}
 
-                <hr/>
-
-                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
 
             </div>
 
