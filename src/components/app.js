31fb8c4e5efe5640bfbfbceee8ba4e6d311b501f
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-04 17:26:24 -0500

    redirecting users to the homepage if they enter a route that does not exist

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,7 @@ import About from './pages/about'
 import Contact from './pages/contact'
 import Blog from './pages/blog'
 import PortfolioDetail from './portfolio/portfolio-detail'
+import NoMatch from './pages/no-match'
 
 
 export default class App extends Component {
@@ -32,7 +33,8 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
-              <Route path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
             </Switch>  
 
           </div>
