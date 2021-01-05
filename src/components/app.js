519f9e65095ae5db656a9b7be3439f9b71275f04
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-05 17:51:44 -0500

    introducing navbar with added styles for the nav-links, integrating SCSS variables to manage the Portfolio's style guide

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import moment from 'moment';
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 
@@ -20,15 +18,10 @@ export default class App extends Component {
   render() {
 
     return (
-      <div className='app'>
+      <div className='container'>
         <Router>
 
           <div>
-            <h1>Matthew Ricklef's Portfolio</h1>
-        
-            <div>
-              {moment().format('MMMM Do YYYY, h:mm:ss a')}
-            </div>
 
             <NavigationComponent />
 
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -78,7 +78,9 @@ export default class PortfolioContainer extends Component {
                     Enterprise
                 </button>
                    
-                {this.portfolioItems()}
+                <div className="portfolio-items-wrapper">
+                    {this.portfolioItems()}
+                </div>
             </div>
         )
     }
