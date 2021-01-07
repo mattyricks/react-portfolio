bcec7ad5193a12a3d061434406040573776bc6c6
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-07 17:50:02 -0500

    overview of react refs to clear the form, need to fix bug with clearing the image uploaders

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -28,6 +28,10 @@ export default class PortfolioForm extends Component {
         this.handleBannerDrop = this.handleBannerDrop.bind(this)
         this.handleLogoDrop = this.handleLogoDrop.bind(this)
 
+        this.thumbRef = React.createRef()
+        this.bannerRef = React.createRef()
+        this.logoRef = React.createRef()
+
 
     }
 
@@ -101,7 +105,23 @@ export default class PortfolioForm extends Component {
         )
         .then(response => {
             this.props.handleSuccessfulFormSubmission(response.data.portfolio_item)
+
+            this.setState({
+                name: "",
+                description: "",
+                category: "eCommerce",
+                position: "",
+                url: "",
+                thumb_image: "",
+                banner_image: "",
+                logo: "",
+            })
+
+            [this.thumbRef, this.bannerRef, this.logoRef].forEach(ref => {
+                ref.current.dropzone.removeAllFiles();
+            })   
         })
+
         .catch(error => {
             console.log("portfolio form handleSubmit error", error)
         })
@@ -172,22 +192,25 @@ export default class PortfolioForm extends Component {
 
                     <div className="image-uploaders">
                         <DropzoneComponent 
+                            ref={this.thumbRef}
                             config = {this.componentConfig()}
-                            djsConfig = {this.djsConfig}
+                            djsConfig = {this.djsConfig()}
                             eventHandlers={this.handleThumbDrop()}
                         >
                         </ DropzoneComponent>
 
                         <DropzoneComponent 
+                            ref={this.bannerRef}
                             config = {this.componentConfig()}
-                            djsConfig = {this.djsConfig}
+                            djsConfig = {this.djsConfig()}
                             eventHandlers={this.handleBannerDrop()}
                         >
                         </ DropzoneComponent>
 
                         <DropzoneComponent 
+                            ref={this.logoRef}
                             config = {this.componentConfig()}
-                            djsConfig = {this.djsConfig}
+                            djsConfig = {this.djsConfig()}
                             eventHandlers={this.handleLogoDrop()}
                         >
                         </ DropzoneComponent>
