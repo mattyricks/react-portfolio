192cc9b9cf3cfeb7629490e37c881bc393ce0e46
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-13 17:20:03 -0500

    finalizing the infinite scroll feature

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -21,11 +21,16 @@ class Blog extends Component {
 
     activateInfiniteScroll() {
         window.onscroll = () => {
+            if(this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+                return;
+            }
+
           if (
             window.innerHeight + document.documentElement.scrollTop ===
             document.documentElement.offsetHeight
           ) {
-            console.log("get more posts");
+              this.getBlogItems()
+            
           }
         };
       }
@@ -34,13 +39,16 @@ class Blog extends Component {
         this.setState({
             currentPage: this.state.currentPage + 1
         })
-        axios.get("https://ricklefsmatthew.devcamp.space/portfolio/portfolio_blogs", 
-            {withCredentials: true
+        axios.get(`https://ricklefsmatthew.devcamp.space/portfolio/portfolio_blogs?page=${this
+            .state.currentPage}`, 
+        {
+            withCredentials: true
         })
         .then(response => {
+            console.log("getting", response.data);
 
             this.setState({
-                blogItems: response.data.portfolio_blogs,
+                blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
                 totalCount: response.data.meta.total_records,
                 isLoading: false
             })       
