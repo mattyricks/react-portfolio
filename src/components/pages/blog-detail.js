ca003ff5448e47e1ee1325b945e98c96995cdf0f
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-13 13:37:23 -0500

    rendering the blog details onto the screen

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -12,11 +12,14 @@ export default class BlogDetail extends Component {
     }
 
         getBlogItem() {
-            axios.get(`https://ricklefsmatthew.devcamp.space/portfolio/portfolio_blogs/${this.state
+            axios
+            .get(`https://ricklefsmatthew.devcamp.space/portfolio/portfolio_blogs/${this.state
                 .currentId}`
             )
             .then(response => {
-                console.log('response', response)
+                this.setState({
+                    blogItem: response.data.portfolio_blog
+                })
             })
             .catch(error => {
                 console.log('getBlogItem error', error);               
@@ -28,11 +31,21 @@ export default class BlogDetail extends Component {
         }
 
     render() {
-        console.log('currentId', this.state.currentId);
+        //destructuring 
+        const {
+            title,
+            content,
+            featured_image_url,
+            blog_status
+        } = this.state.blogItem
         
         return (
             <div>
-                <h1>Blog Detail</h1>
+                <h1>{title}</h1>
+                <img src={featured_image_url} />
+                <div>
+                    {content}
+                </div>
             </div>
         )
     }
