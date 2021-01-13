3497c55c48c26181404e8b78ed7e7e2636cda5e5
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-13 13:23:45 -0500

    calling a single blog item from the API

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class BlogDetail extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            currentId: this.props.match.params.slug,
+            blogItem: {}
+        }
+    }
+
+        getBlogItem() {
+            axios.get(`https://ricklefsmatthew.devcamp.space/portfolio/portfolio_blogs/${this.state
+                .currentId}`
+            )
+            .then(response => {
+                console.log('response', response)
+            })
+            .catch(error => {
+                console.log('getBlogItem error', error);               
+            })
+        }
+
+        componentDidMount() {
+            this.getBlogItem()
+        }
+
     render() {
+        console.log('currentId', this.state.currentId);
+        
         return (
             <div>
                 <h1>Blog Detail</h1>
