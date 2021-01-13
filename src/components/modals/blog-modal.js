cd0f267a37f7f4e0f5483ab4f4fd22931b065eb2
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-13 18:47:25 -0500

    implementing styles to format the Modal and adding an overlay to affect the backgroundColor

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -5,11 +5,27 @@ export default class BlogModal extends Component {
     constructor(props) {
         super(props)
 
+        this.customStyles = {
+            content: {
+                top: "50%",
+                left: "50%",
+                right: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%, -50%)",
+                width: "800px"
+            },
+            overlay: {
+                backgroundColor: 'rgba(1, 1, 1, 0.75)'
+            }
+        }
+
     }
     render() {
         return (
             <div>
-                <ReactModal onRequestClose={()  => {
+                <ReactModal 
+                    style={this.customStyles}
+                    onRequestClose={()  => {
                     this.props.handleModalClose();
                 }}
                 isOpen={this.props.modalIsOpen}
