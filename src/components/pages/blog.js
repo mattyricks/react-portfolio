ea38dfd736949cf7f1a939ddbaeb35a94dc6a45c
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-14 18:17:09 -0500

    transforming Blog Modal link into a FontAwesomeIcon and then using absolute postion to keep icon fixed on bottom right side of screen

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -106,7 +106,9 @@ class Blog extends Component {
 
             <div className="new-blog-link">
                 <a onClick={this.handleNewBlogClick}>
-                    Open Modal!
+                    <FontAwesomeIcon
+                        icon="plus-circle"
+                    />
                 </a>
             </div>
 
