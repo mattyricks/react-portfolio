65998dac76ddead2a0860441807fb7ab2fef8d8d
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-15 18:58:22 -0500

    converting image files into Base64 Strings

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -13,22 +13,35 @@ export default class RichTextEditor extends Component {
         }
 
         this.onEditorStateChange = this.onEditorStateChange.bind(this)
+        this.getBase64 = this.getBase64.bind(this)
+        this.uploadFile = this.uploadFile.bind(this)
     }
 
     // created this function.
     onEditorStateChange(editorState) {
         this.setState(
             { editorState },
-            //when somebody starts typing we then pass in the new editorState
+            // when somebody starts typing we then pass in the new editorState
             this.props.handleRichTextEditorChange(
                 draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
             )
         )
     }
 
-    uploadFile(file) {
-        console.log("upload file", file)
+    getBase64(file, callback) {
+        let reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = () => callback(reader.result)
+        reader.onerror = error => {}
     }
+
+    uploadFile(file) {
+        return new Promise((resolve, reject) => {
+            this.getBase64(file, data => resolve({data: { link: data } }))
+        })
+    }
+
+
 
     render() {
         return (
