0a174ecd24783daa71280fb9d76523d5e08df700
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-18 10:46:49 -0500

    debugging homepage styles

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,102 +1,98 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import ReactHtmlParser from "react-html-parser"
-import BlogFeaturedImage from '../blog/blog-featured-image'
-import BlogForm  from '../blog/blog-form'
+import React, { Component } from "react";
+import axios from "axios";
+import ReactHtmlParser from "react-html-parser";
+import BlogFeaturedImage from "../blog/blog-featured-image";
+import BlogForm from "../blog/blog-form";
 
 export default class BlogDetail extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            currentId: this.props.match.params.slug,
-            blogItem: {},
-            editMode: false,
-        }
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {},
+      editMode: false,
+    };
 
-        this.handleEditClick = this.handleEditClick.bind(this)
-        this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this)
-        this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(this)
+    this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+    this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(
+      this
+    );
+  }
+
+  handleUpdateFormSubmission(blog) {
+    this.setState({
+      blogItem: blog,
+      editMode: false,
+    });
+  }
+
+  handleFeaturedImageDelete() {
+    this.setState({
+      blogItem: {
+        featured_image_url: "",
+      },
+    });
+  }
+
+  handleEditClick() {
+    if (this.props.loggedInStatus === "LOGGED_IN") {
+      this.setState({ editMode: true });
     }
+  }
 
-    handleUpdateFormSubmission(blog) {
+  getBlogItem() {
+    axios
+      .get(
+        `https://ricklefsmatthew.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+      )
+      .then((response) => {
         this.setState({
-            blogItem: blog,
-            editMode: false
-        })
-    }
+          blogItem: response.data.portfolio_blog,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItem error", error);
+      });
+  }
 
-    handleFeaturedImageDelete () {
-        this.setState({
-            blogItem: {
-                featured_image_url: ""
-            }
-        })
-    }
+  componentDidMount() {
+    this.getBlogItem();
+  }
 
-    handleEditClick () {
-        if (this.props.loggedInStatus === "LOGGED_IN") {
-        this.setState({ editMode: true })
-        }
-    }
+  render() {
+    //destructuring
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status,
+    } = this.state.blogItem;
 
-        getBlogItem() {
-            axios
-            .get(`https://ricklefsmatthew.devcamp.space/portfolio/portfolio_blogs/${this.state
-                .currentId}`
-            )
-            .then(response => {
-                this.setState({
-                    blogItem: response.data.portfolio_blog
-                })
-            })
-            .catch(error => {
-                console.log('getBlogItem error', error);               
-            })
-        }
-
-        componentDidMount() {
-            this.getBlogItem()
-        }
-
-    render() {
-        //destructuring 
-        const {
-            title,
-            content,
-            featured_image_url,
-            blog_status
-        } = this.state.blogItem
-
-        const contentManager = () => {
-            if (this.state.editMode) {
-                return <BlogForm 
-                handleFeaturedImageDelete={this.handleFeaturedImageDelete}
-                handleUpdateFormSubmission={this.handleUpdateFormSubmission}
-                editMode={this.state.editMode} 
-                blog={this.state.blogItem}
-            />
-            } else {
-                return (
-                    <div className="content-container">
-
-                        <h1 onClick={this.handleEditClick}>{title}</h1>
-
-                        <BlogFeaturedImage img={featured_image_url} />
-                        
-                        <div className="content">
-                            {ReactHtmlParser(content)}
-                        </div>
-
-                    </div>
-                )
-            }
-        }
-        
+    const contentManager = () => {
+      if (this.state.editMode) {
         return (
-            <div className="blog-container">
-                {contentManager()}
-            </div>
-        )
-    }
+          <BlogForm
+            handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+            handleUpdateFormSubmission={this.handleUpdateFormSubmission}
+            editMode={this.state.editMode}
+            blog={this.state.blogItem}
+          />
+        );
+      } else {
+        return (
+          <div className="content-container">
+            <h1 onClick={this.handleEditClick}>{title}</h1>
+
+            <BlogFeaturedImage img={featured_image_url} />
+
+            <div className="content">{ReactHtmlParser(content)}</div>
+          </div>
+        );
+      }
+    };
+
+    return <div className="blog-container">{contentManager()}</div>;
+  }
 }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,119 +1,103 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            isLoading: false,
-            data: []
-        };
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: [],
+    };
 
-        this.handleFilter = this.handleFilter.bind(this)
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+      this.getPortfolioItems(filter);
     }
+  }
 
-    handleFilter(filter) {
-        if(filter === "CLEAR_FILTERS") {
-            this.getPortfolioItems()
+  getPortfolioItems(filter = null) {
+    axios
+      .get("https://ricklefsmatthew.devcamp.space/portfolio/portfolio_items")
+      .then((res) => {
+        if (filter) {
+          this.setState({
+            data: response.data.portfolio_items.filter((item) => {
+              return item.category === filter;
+            }),
+          });
         } else {
-            this.getPortfolioItems(filter)
+          this.setState({
+            data: res.data.portfolio_items,
+          });
         }
-    }
-    
-
-    getPortfolioItems(filter = null) {
-        axios
-          .get('https://ricklefsmatthew.devcamp.space/portfolio/portfolio_items')
-          .then(res => {
-              if (filter) {
-                this.setState({
-                    data: response.data.portfolio_items.filter(item => {
-                        return item.category === filter;
-                    })
-                })
-              } else {
-                this.setState({
-                    data: res.data.portfolio_items
-                })
-              }
-
-
       })
-        .catch(error => {
+      .catch((error) => {
         // handle error
-          console.log(error);
+        console.log(error);
       })
-        .then(function () {
+      .then(function () {
         // always executed
       });
+  }
+
+  portfolioItems() {
+    return this.state.data.map((item) => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    portfolioItems() {
+    return (
+      <div className="homepage-wrapper">
+        <div className="filter-links">
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("eCommerce")}
+          >
+            eCommerce
+          </button>
 
-        return this.state.data.map(item => {
-            return (
-                <PortfolioItem
-                 key= {item.id}
-                 item= {item}
-                />
-            );
-        });
-    }
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Scheduling")}
+          >
+            Scheduling
+          </button>
 
-    componentDidMount() {
-        this.getPortfolioItems();
-    }
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Enterprise")}
+          >
+            Enterprise
+          </button>
 
-    render() { 
-        if (this.state.isLoading) {
-            return <div>Loading...</div>
-        }
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("CLEAR_FILTERS")}
+          >
+            ALL
+          </button>
+        </div>
 
-        return (
-
-            <div className="homepage-wrapper">
-
-                <div className="filter-links">
-
-                    <h2>{this.state.pageTitle}</h2>     
-
-                    <div className="portfolio-items-wrapper">
-
-                        <button 
-                            className="btn"
-                            onClick={() => this.handleFilter('eCommerce')}>
-                                eCommerce
-                        </button>
-
-                        <button
-                            className="btn"
-                            onClick={() => this.handleFilter('Scheduling')}>
-                                Scheduling
-                        </button>
-
-                        <button
-                            className="btn"
-                            onClick={() => this.handleFilter('Enterprise')}>
-                                Enterprise
-                        </button>
-
-                        <button
-                            className="btn"
-                            onClick={() => this.handleFilter('CLEAR_FILTERS')}>
-                                ALL
-                        </button>
-
-                        {this.portfolioItems()}
-
-                    </div>
-
-                </div>
-
-            </div>
-        )
-    }
+        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+      </div>
+    );
+  }
 }
