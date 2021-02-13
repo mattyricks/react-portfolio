4e322c38b84df6f23b58a1cd36b46d3fbdf53593
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-02-13 12:37:49 -0500

    updates contact and about me sections

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -20,7 +20,7 @@ export default function () {
               <FontAwesomeIcon icon="phone" />
             </div>
 
-            <div className="text">555-555-5555</div>
+            <div className="text">619-490-8315</div>
           </div>
 
           <div className="bullet-point-group">
